245ac75b46ab32514b946a25a2f7725163fa36a5
M. Cihan Ozer <ozer.mc@gmail.com>
2018-02-02 16:31:52 +0300

    Create AbstractAgentBuilder.hpp

---
+++ Builder/AbstractAgentBuilder.hpp
@@ -0,0 +1,34 @@
+//
+//  AbstractAgentBuilder.hpp
+//
+//  Created by Cihan Ozer on 01/02/2018.
+//
+
+#ifndef AbstractAgentBuilder_hpp
+#define AbstractAgentBuilder_hpp
+
+#include "Agent.hpp"
+
+// Abstract Agent builder
+class AbstractAgentBuilder
+{
+public:
+    
+    virtual ~AbstractAgentBuilder() {}
+    
+    Agent* getAgent();
+    
+    // Where you will init the smart pointer
+    virtual void createNewAgent() = 0;
+    
+    virtual void setKickinChance() = 0;
+    virtual void setBallControlChance() = 0;
+    virtual void setBeatingChance() = 0;
+    
+protected:
+    
+    std::unique_ptr<Agent> mAgentSptr;
+    
+};
+
+#endif /* AbstractAgentBuilder_hpp */
